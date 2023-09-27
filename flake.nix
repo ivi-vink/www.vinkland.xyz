f38d4b768d51cbe70a8bd1a56584d6202b57556f
Mike Vink <mike1994vink@gmail.com>
2023-09-27 18:47:44 +0200

    hugo init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    nix-filter.url = "github:numtide/nix-filter";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs =
+        import inputs.nixpkgs
+        {
+          inherit system;
+        };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "www.vinkland.xyz";
+        buildInputs = [
+          pkgs.hugo
+        ];
+      };
+    });
+}
